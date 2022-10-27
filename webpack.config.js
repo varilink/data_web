5814d5a27b926df7c6c1a018655ed7891745f8fa
David Williamson <david.williamson@varilink.co.uk>
2022-10-27 23:19:55 +0100

    ADD DOCS TO ASSETS AND UPDATE WEBPACK TO INCLUDE THEM IN THE BUILD
    The documents add were already distributed to the live site. The webpack
    configuration merely copies them over to dist/ just as it does for the
    contents of the img/ folder.

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ module.exports = [
       new CopyPlugin({
         patterns: [
           { from: 'src/img/', to: '../img' },
+          { from: 'src/docs/', to: '../docs' },
         ],
       }),
       new MiniCssExtractPlugin(),
